0c78c99bc15b9c5d3aedf364d754a11c187b9a99
Changbin Liu <changbin.liu@gmail.com>
2013-05-15 15:11:11 -0400

    add meta project Info
    * tox.ini and tools (requires, pep8, pyflaks)
    
    * Authors
    
    * modify a bit of README
    
    * add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# Copyright (c) 2012 AT&T. All right reserved.
+#
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = '0.0.1'
+
+setup(
+    name='inception',
+    version=version,
+    description="Inception: Towards a Nested Cloud Architecture",
+    license="Apache 2.0",
+    classifiers=["Programming Language :: Python"],
+    url='https://github.com/maoy/inception',
+    packages=["inception"],
+    )
---
+++ tools/pep8.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+pep8 inception/
