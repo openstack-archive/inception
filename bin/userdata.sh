327f22fcde0f07aa288380fb99cccf87ff9461a6
Yun Mao <yunmao@gmail.com>
2013-05-24 16:02:23 -0400

    Automatically partition /dev/vdb
    Change-Id: Idd76f96aa41504a012e8cb48439d7a5f908bbc2d

--- bin/userdata.sh
+++ bin/userdata.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# umount extra volume not needed by OpenStack
+# /dev/vdb is formatted and mounted on /mnt
+# we need to use it as a volume for cinder instead
 sudo /bin/umount /mnt/
+sudo parted /dev/vdb --script -- mklabel gpt
+sudo parted /dev/vdb --script -- mkpart primary ext4 1 -1
+sudo parted /dev/vdb --script -- set 1 lvm on
 
 # use dnsmasq (fixed resolv.conf) instead
 sudo apt-get -y remove resolvconf
