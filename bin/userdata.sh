181f2141d99608e0c24fda442eb721bea8743a13
Changbin Liu <changbin.liu@gmail.com>
2013-06-10 11:18:02 -0400

    Fix /dev/sdb mount issue upon reboot
    /etc/fstab modification is put in userdata.sh
    
    Change-Id: Id3b330cd2a61eb105c548343459274a79eb5d586

--- bin/userdata.sh
+++ bin/userdata.sh
@@ -3,6 +3,7 @@
 # /dev/vdb is formatted and mounted on /mnt
 # we need to use it as a volume for cinder instead
 sudo /bin/umount /mnt/
+sudo sed -i 's/\/dev\/vdb/#\/dev\/vdb/g' /etc/fstab
 sudo parted /dev/vdb --script -- mklabel gpt
 sudo parted /dev/vdb --script -- mkpart primary ext4 1 -1
 sudo parted /dev/vdb --script -- set 1 lvm on
