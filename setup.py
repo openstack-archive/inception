03ed5b7e9fa056b7ecc612914c6d43f472659cd3
Andrew Forrest <forrest@research.att.com>
2013-06-28 14:48:13 -0700

    Incoporate oslo.config facilities in Inception
    Convert Inception code to use a configuration file and command line options as
    processed by oslo.config.
    
    + orchestrator modified to use oslo.config for options
    + sample config file introduced
    + dependency on oslo.config added to setup.py
    + version string moved from setup.py to module init for ready access
      for --version and others
    
    Implements: blueprint Inception Configuration Blueprint
    Change-Id: I984700867d8ddf5ee1602cc4ed6608f36a38a6be

--- setup.py
+++ setup.py
@@ -7,17 +7,20 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = '0.0.1'
+
+# move version string out of setup so it is readily available to others
+from inception import __version__
 
 setup(
     name='inception',
-    version=version,
+    version=__version__,
     description="Inception: Towards a Nested Cloud Architecture",
     license="Apache 2.0",
     classifiers=["Programming Language :: Python"],
     url='https://github.com/maoy/inception',
     packages=["inception"],
     install_requires=[
+        "oslo.config>=1.1.1",
         "python-novaclient>=2.13.0",
         "IPython>=0.13.2",
     ],
