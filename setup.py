b6cd87028518bbbad580a90972b31bba588d765a
Changbin Liu <changbin.liu@gmail.com>
2013-07-08 21:00:40 -0400

    Cleanup oslo.config
    - Remove config opts and orchestrator ctor document redundancy
    
    - Make "atomic" an attribute of orchestrator
    
    - A bit reordering of opts
    
    - Cleanup and rename inception.conf.sample
    
    - Some other minors: Authors, setup.py
    
    Change-Id: I9c501acf4a11c2fc0056130bd163bbccd2ec9e24

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # Copyright (c) 2012 AT&T. All right reserved.
-#
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-
 
 # move version string out of setup so it is readily available to others
 from inception import __version__
@@ -17,7 +15,7 @@ setup(
     description="Inception: Towards a Nested Cloud Architecture",
     license="Apache 2.0",
     classifiers=["Programming Language :: Python"],
-    url='https://github.com/maoy/inception',
+    url='https://github.com/stackforge/inception',
     packages=["inception"],
     install_requires=[
         "oslo.config>=1.1.1",
